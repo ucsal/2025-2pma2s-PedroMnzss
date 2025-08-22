e312997090821729bff3ef14cd18415e2722407d
Pedro Vilas Boas Menezes <pedrovbm2006@gmail.com>
2025-08-22 12:18:00 -0300

    Implementação das classes TaxCalculator e ReceiptFormatter, e ajustes nas classes novas e antigas para que sigam a SRP

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,19 +1,22 @@
 package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
-	public String generate(Order order) {
-		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
-		double total = subtotal + tax;
-		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
-		sb.append("=== RECIBO ===\n");
-		for (var i : order.getItems()) {
-			sb.append(i.getName()).append(" x").append(i.getQuantity()).append(" = ").append(i.getUnitPrice() * i.getQuantity())
-					.append("\n");
-		}
-		sb.append("Subtotal: ").append(subtotal).append("\n");
-		sb.append("Tax: ").append(tax).append("\n");
-		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
+	private double subtotal;
+	private double tax;
+	private double total;
+
+	public String generate(Order order){
+		TaxCalculator tc = new TaxCalculator();
+		this.subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
+		this.tax = tc.CalculateTax(subtotal);
+		this.total = subtotal + tax;
+
+
+		ReceiptFormatter rf = new ReceiptFormatter();
+		return rf.format(order, tax, subtotal, total);
 	}
+	public double getSubtotal(){
+		return this.subtotal;
+	}
+	
 }
